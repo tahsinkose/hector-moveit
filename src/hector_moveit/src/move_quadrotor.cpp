5cc6bb50d4108b1189edae341536a7c94922936c
Tahsincan Köse <darthrainos50@gmail.com>
2018-08-12 16:49:50 +0300

    Introduce Planner Class with smart pointers

--- src/hector_moveit/src/move_quadrotor.cpp
+++ src/hector_moveit/src/move_quadrotor.cpp
@@ -6,20 +6,53 @@
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
 #include <nav_msgs/Odometry.h>
-std::vector<double> odom(7);
-bool odom_received = false;
+class QuadrotorPlanner{
+    private:
+        std::unique_ptr<moveit::planning_interface::MoveGroupInterface> move_group;
+        std::unique_ptr<robot_state::RobotState> start_state;
+        bool odom_received;
+        std::vector<double> odometry_information;
+        ros::Subscriber base_sub;
+        void poseCallback(const nav_msgs::Odometry::ConstPtr & msg){
+            odometry_information[0] = msg->pose.pose.position.x;
+            odometry_information[1] = msg->pose.pose.position.y;
+            odometry_information[2] = msg->pose.pose.position.z;
 
-void poseCallback(const nav_msgs::Odometry::ConstPtr & msg){
-    odom[0] = msg->pose.pose.position.x;
-    odom[1] = msg->pose.pose.position.y;
-    odom[2] = msg->pose.pose.position.z;
+            odometry_information[3] = msg->pose.pose.orientation.x;
+            odometry_information[4] = msg->pose.pose.orientation.y;
+            odometry_information[5] = msg->pose.pose.orientation.z;
+            odometry_information[6] = msg->pose.pose.orientation.w;
+            odom_received = true;
+        }
+    public:
+        QuadrotorPlanner(const std::string PLANNING_GROUP,ros::NodeHandle& nh){
+            odometry_information.resize(7);
+            base_sub = nh.subscribe<nav_msgs::Odometry>("/ground_truth/state",10,&QuadrotorPlanner::poseCallback,this);
+            move_group.reset(new moveit::planning_interface::MoveGroupInterface(PLANNING_GROUP));
+            robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+            robot_model::RobotModelPtr kmodel = robot_model_loader.getModel();
 
-    odom[3] = msg->pose.pose.orientation.x;
-    odom[4] = msg->pose.pose.orientation.y;
-    odom[5] = msg->pose.pose.orientation.z;
-    odom[6] = msg->pose.pose.orientation.w;
-    odom_received = true;
-}
+            move_group->setPlannerId("RRTConnectkConfigDefault");
+            move_group->setNumPlanningAttempts(10);
+            move_group->setWorkspace(-50,-50,-50,50,50,50);
+            
+            start_state.reset(new robot_state::RobotState(move_group->getRobotModel()));
+        }
+        void run(){
+
+            std::vector<double> test_joint_values = {0,0,10,0,0,0,1};
+            move_group->setJointValueTarget(test_joint_values);
+            moveit::planning_interface::MoveGroupInterface::Plan plan;
+            while(!odom_received)
+                ;
+            start_state->setVariablePositions(odometry_information);
+            move_group->setStartState(*start_state);
+            bool success = (move_group->plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+            ROS_INFO_NAMED("Trial", "%s", success ? "SUCCESS" : "FAILED");
+        }
+};
+
+
 
 int main(int argc, char** argv)
 {
@@ -28,27 +61,8 @@ int main(int argc, char** argv)
     ros::AsyncSpinner spinner(1);
     spinner.start();
     ros::NodeHandle node_handle("~");
-    ros::Subscriber base_sub = node_handle.subscribe<nav_msgs::Odometry>("/ground_truth/state",10,&poseCallback);
     const std::string PLANNING_GROUP = "DroneBody";
-    robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
-    robot_model::RobotModelPtr kmodel = robot_model_loader.getModel();
-    
-    std::vector<double> test_joint_values = {0,0,10,0,0,0,1};
-    moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
-
-    move_group.setPlannerId("RRTConnectkConfigDefault");
-    move_group.setNumPlanningAttempts(10);
-    move_group.setWorkspace(-50,-50,-50,50,50,50);
-    
-    move_group.setJointValueTarget(test_joint_values);
-    moveit::planning_interface::MoveGroupInterface::Plan plan;
-    robot_state::RobotState start_state(move_group.getRobotModel());
-
-    while(!odom_received)
-        ;
-    start_state.setVariablePositions(odom);
-    move_group.setStartState(start_state);
-    bool success = (move_group.plan(plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
-    ROS_INFO_NAMED("Trial", "%s", success ? "SUCCESS" : "FAILED");
+    QuadrotorPlanner planner(PLANNING_GROUP,std::ref(node_handle));
+    planner.run();
     return 0;
 }

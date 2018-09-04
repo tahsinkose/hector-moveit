4b12114b1eccda6da8e152530eb3d77073bafc08
Tahsincan Köse <darthrainos50@gmail.com>
2018-09-04 23:05:09 +0300

    Introduce submodules.

--- src/hector_quadrotor-kinetic-devel/hector_quadrotor_model/matlab/compile_all.m
+++
@@ -1,8 +0,0 @@
-rtw_config = coder.config('LIB');
-rtw_config.TargetLang = 'C';
-rtw_config.CodeReplacementLibrary = 'C89/C90 (ANSI)';
-rtw_config.CCompilerOptimization = 'On';
-
-codegen -config rtw_config -v quadrotorPropulsion motorspeed -o quadrotorPropulsion
-codegen -config rtw_config -v quadrotorDrag -o quadrotorDrag
-
